e5429f39ca4282f5176b30be28bbc2206607503f
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-12-04 18:20:25 -0500

    Make hseq a subtype of seq instead of tuple.

--- hseq.v
+++ hseq.v
@@ -5,8 +5,6 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Section HSeq.
-
 Section Def.
 
 Variable I : eqType.
@@ -16,7 +14,7 @@ Section Basic.
 Variables (T_ : I -> Type) (idx : seq I).
 
 Record hseq : predArgType :=
-  HSeq {hsval :> (size idx).-tuple {i : I & T_ i}; _ : map tag hsval == idx}.
+  HSeq {hsval :> seq {i : I & T_ i}; _ : map tag hsval == idx}.
 
 Canonical hseq_subType := [subType for hsval].
 
@@ -42,46 +40,65 @@ Canonical hseq_countType (I : countType) (T_ : I -> countType) idx :=
   CountType (hseq T_ idx) (hseq_countMixin T_ idx).
 Canonical hseq_subCountType (I : countType) (T_ : I -> countType) idx :=
   [subCountType of hseq T_ idx].
-Definition hseq_finMixin (I : finType) (T_ : I -> finType) idx :=
-  [finMixin of hseq T_ idx by <:].
-Canonical hseq_finType (I : finType) (T_ : I -> finType) idx :=
-  FinType (hseq T_ idx) (hseq_finMixin T_ idx).
-Canonical hseq_subFinType (I : finType) (T_ : I -> finType) idx :=
-  [subFinType of hseq T_ idx].
 
-Lemma card_hseq (I : finType) (T_ : I -> finType) (idx : seq I) :
-  #|hseq T_ idx| = foldr muln 1 [seq #|T_ i| | i <- idx].
+Module Type FinHSeqSig.
+Section FinHSeqSig.
+Variables (I : finType) (T_ : I -> finType) (idx : seq I).
+Parameter enum : seq (hseq T_ idx).
+Axiom enumP : Finite.axiom enum.
+Axiom size_enum : size enum = foldr muln 1 [seq #|T_ i| | i <- idx].
+End FinHSeqSig.
+End FinHSeqSig.
+
+Module FinHSeq.
+Section FinHSeq.
+Variables (I : finType) (T_ : I -> finType) (idx : seq I).
+
+Definition enum : seq (hseq T_ idx) :=
+  let extend i e := flatten (codom (fun x : T_ i => map (cons (Tagged T_ x)) e)) in
+  pmap insub (foldr extend [::[::]] idx).
+
+Lemma enumP : Finite.axiom enum.
 Proof.
-rewrite card_sub; elim: idx=> [|i idx IH] /=.
-  by rewrite (@eq_card1 _ [tuple]) // => x /=; rewrite !inE tuple0.
-pose X := {i' : I & T_ i'}.
-have -> : #|T_ i| = #|[pred x : X | tag x == i]|.
-  pose in_X (t : T_ i) := Tagged T_ t.
-  have in_X_inj : injective in_X.
-    move=> x y /eqP; rewrite /in_X -tag_eqE /tag_eq /= tagged_asE.
-    by case/andP => [_ /eqP].
-  rewrite -(card_image in_X_inj); apply: eq_card=> x; rewrite !inE.
-  apply/(sameP idP)/(iffP idP).
-    case: x => [i' x] /= /eqP Hi'; rewrite {}Hi' {i'} in x *.
-    by rewrite mem_image //.
-  by move: x => _ /imageP [x Hx ->]; rewrite eqxx.
-rewrite -{}IH -cardX /=.
-pose f (t : (size idx).+1.-tuple X) := (thead t, [tuple of behead t]).
-have f_inj : injective f.
-  move=> t1 t2 /=; case/tupleP: t1=> [x1 t1]; case/tupleP: t2=> [x2 t2] /=.
-  by rewrite /f !theadE=> - [-> E]; apply: val_inj=> /=; rewrite E.
-rewrite -(card_image f_inj); apply: eq_card=> - [x t] /=; rewrite !inE /=.
-have -> : (x, t) = f [tuple of x :: t].
-  by rewrite /f /= theadE; congr pair; apply: val_inj.
-by rewrite mem_image.
+case=> /= hs Phs; rewrite -(count_map _ (pred1 hs)) (pmap_filter (@insubK _ _ _)).
+rewrite count_filter -(@eq_count _ (pred1 hs)) => [|s /=]; last first.
+  by rewrite isSome_insub; case: eqP=> // ->.
+elim: idx hs Phs => [|i idx' IH] [|[i' x] s] //= /eqP [Hi {IH}/eqP/IH].
+rewrite {}Hi {i'} in x *; move: (foldr _ _ _) => eidx IH.
+transitivity (x \in T_ i : nat)=> //.
+rewrite -mem_enum codomE.
+elim: (fintype.enum (T_ i)) (enum_uniq (T_ i))=> //= y e IHe /andP[/negPf ney].
+rewrite count_cat count_map inE /preim /= {1}/eq_op /= eq_sym => {IHe}/IHe->.
+rewrite -tag_eqE /tag_eq eqxx /= tagged_asE.
+by case: eqP => [->|_]; rewrite ?(ney, count_pred0, IH).
 Qed.
+
+Lemma size_enum : size enum = foldr muln 1 [seq #|T_ i| | i <- idx].
+Proof.
+rewrite /= size_pmap_sub; elim: idx=> [|i idx' /= <-] //=.
+rewrite cardE /codom /image_mem; move: (foldr _ _ _) => e.
+elim: (fintype.enum (T_ i)) => //= x xs IHxs.
+rewrite count_cat {}IHxs count_map mulSn /preim /=; congr addn.
+by apply: eq_count=> s /=; rewrite eqE /= eqxx.
+Qed.
+
+End FinHSeq.
+End FinHSeq.
+
+Section UseFinHSeq.
+
+Variables (I : finType) (T_ : I -> finType) (idx : seq I).
+
+Definition hseq_finMixin := FinMixin (@FinHSeq.enumP I T_ idx).
+Canonical hseq_finType := Eval hnf in FinType (hseq T_ idx) hseq_finMixin.
+Canonical hseq_subFinType := [subFinType of hseq T_ idx].
+
+End UseFinHSeq.
 
 (* FIXME: find a better name for this *)
 Definition mkhseq (I : eqType) T_ (idx : seq I) (hs : hseq T_ idx)
                   (mkHs : map tag hs == idx -> hseq T_ idx) :=
   mkHs (let: HSeq _ hsP := hs return map tag hs == idx in hsP).
-
-End HSeq.
 
 Notation "[ 'hseq' 'of' hs ]" :=
   (mkhseq (fun hsP => @HSeq _ _ _ hs hsP))
@@ -114,15 +131,19 @@ elim: idx => /= [|i' idx' <-]; first by rewrite big_ord0.
 by rewrite big_mkcond /= big_ord_recl /= !inE /= -big_mkcond /=.
 Qed.
 
-Lemma hnth_proof T_ (hs : hseq T_ idx) (n : hidx) : tag (tnth hs n) = i.
+Lemma tuple_of_hseq_proof T_ (hs : hseq T_ idx) : size hs == size idx.
+Proof. by rewrite -[in X in _ == X](eqP (valP hs)) size_map. Qed.
+Canonical tuple_of_hseq T_ (hs : hseq T_ idx) := Tuple (tuple_of_hseq_proof hs).
+
+Lemma hnth_proof T_ (hs : hseq T_ idx) (n : hidx) : tag (tnth [tuple of hs] n) = i.
 Proof.
-case: hs n => [t /= /eqP Ht] [n /= /eqP <-] /=.
-rewrite -{2}Ht -(@tnth_nth _ _ _ [tuple of [seq tag i | i <- t]]).
-by rewrite tnth_map.
+case: hs n => [t /= Ht] [n /= /eqP <-] /=.
+rewrite -[in X in nth _ X _](eqP Ht).
+by rewrite -tnth_map /= (tnth_nth i).
 Qed.
 
 Definition hnth T_ (hs : hseq T_ idx) (n : hidx) : T_ i :=
-  eq_rect _ T_ (tagged (tnth hs n)) _ (hnth_proof hs n).
+  eq_rect _ T_ (tagged (tnth [tuple of hs] n)) _ (hnth_proof hs n).
 
 End Def.
 
@@ -133,12 +154,13 @@ Section SeqHSeq.
 Variables (I : eqType) (T_ : I -> Type) (idx : seq I).
 
 Lemma cons_hseqP i (x : T_ i) (hs : hseq T_ idx) :
-  map tag [tuple of Tagged T_ x :: hs] == i :: idx.
+  map tag (Tagged T_ x :: hs) == i :: idx.
 Proof. by rewrite /= tags_hseq. Qed.
+
 Canonical cons_hseq i (x : T_ i) (hs : hseq T_ idx) :=
   @HSeq _ _ (i :: idx) _ (cons_hseqP x hs).
 
 Check (fun i (x : T_ i) (hs : hseq T_ idx) =>
-         [hseq of [tuple of Tagged T_ x :: hs] : (size (i :: _)).-tuple _]).
+         [hseq of Tagged T_ x :: hs]).
 
 End SeqHSeq.
