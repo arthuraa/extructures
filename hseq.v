e838834104c223d4db1848edab0735d7439513e6
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2014-12-04 23:58:04 -0500

    More functions and lemmas for hseq.

--- hseq.v
+++ hseq.v
@@ -100,6 +100,10 @@ Definition mkhseq (I : eqType) T_ (idx : seq I) (hs : hseq T_ idx)
                   (mkHs : map tag hs == idx -> hseq T_ idx) :=
   mkHs (let: HSeq _ hsP := hs return map tag hs == idx in hsP).
 
+Lemma mkhseqE (I : eqType) T_ (idx : seq I) (hs : hseq T_ idx) :
+  mkhseq (fun Phs => @HSeq I T_ idx hs Phs) = hs.
+Proof. by case: hs. Qed.
+
 Notation "[ 'hseq' 'of' hs ]" :=
   (mkhseq (fun hsP => @HSeq _ _ _ hs hsP))
   (at level 0, format "[ 'hseq'  'of'  hs ]") : form_scope.
@@ -147,11 +151,24 @@ Definition hnth T_ (hs : hseq T_ idx) (n : hidx) : T_ i :=
 
 End Def.
 
+Definition hidx0 {I : eqType} {i : I} {idx : seq I} : hidx i (i :: idx) :=
+  HIdx (eqxx _ : nth i (i :: idx) (ord0 : 'I_(size idx).+1) == i).
+
+Definition hshead (I : eqType) (T_ : I -> Type) i idx (hs : hseq T_ (i :: idx))
+                  : T_ i :=
+  hnth hs hidx0.
+
 End HIdx.
 
 Section SeqHSeq.
 
 Variables (I : eqType) (T_ : I -> Type) (idx : seq I).
+
+Canonical nil_hseq (I : eqType) (T_ : I -> Type) :=
+  @HSeq I T_ [::] [::] isT.
+
+Notation "[ 'hseq' ]" := ([hseq of [::]])
+  (at level 0, format "[ 'hseq' ]") : form_scope.
 
 Lemma cons_hseqP i (x : T_ i) (hs : hseq T_ idx) :
   map tag (Tagged T_ x :: hs) == i :: idx.
@@ -165,4 +182,107 @@ Proof. by rewrite map_cat !tags_hseq. Qed.
 Canonical cat_hseq idx' hs (hs' : hseq T_ idx') :=
   HSeq (cat_hseqP hs hs').
 
+Lemma behead_hseqP (hs : hseq T_ idx) : map tag (behead hs) == behead idx.
+Proof. by rewrite -behead_map tags_hseq. Qed.
+Canonical behead_hseq hs := HSeq (behead_hseqP hs).
+
 End SeqHSeq.
+
+Lemma hseq_nil (I : eqType) (T_ : I -> Type) :
+  all_equal_to ([hseq of [::]] : hseq T_ [::]).
+Proof. by move=> [[|??] Phs]; apply: val_inj=> //=. Qed.
+
+CoInductive hseq_cons_spec (I : eqType) (T_ : I -> Type) i idx
+  : hseq T_ (i :: idx) -> Type :=
+  HSeqConsSpec x (hs : hseq T_ idx)
+  : hseq_cons_spec [hseq of Tagged T_ x :: hs].
+
+Lemma hseqP (I : eqType) (T_ : I -> Type) i idx (hs : hseq T_ (i :: idx))
+  : hseq_cons_spec hs.
+Proof.
+case: hs => [[|[i' x] s] // Phs].
+have Hi : i' = i by case/andP: Phs => [/eqP Hi _].
+rewrite {}Hi {i'} in x Phs *.
+have Ps : map tag s == idx by move: (eqP Phs) => /= [->].
+pose hs := HSeq Ps.
+rewrite (_ : @HSeq _ _ (i :: idx) _ Phs = [hseq of Tagged T_ x :: hs]) //.
+by apply: val_inj.
+Qed.
+
+Lemma hsheadE (I : eqType) (T_ : I -> Type) i idx
+              (x : T_ i) (hs : hseq T_ idx) :
+  hshead [hseq of Tagged T_ x :: hs] = x.
+Proof. by rewrite /hshead /= /hnth eq_axiomK /=. Qed.
+
+Lemma hseq_eta (I : eqType) (T_ : I -> Type) i idx :
+  forall hs : hseq T_ (i :: idx),
+    hs = [hseq of Tagged T_ (hshead hs) :: behead hs].
+Proof.
+by move=> hs; case/hseqP: hs => x hs; rewrite hsheadE /=; apply: val_inj.
+Qed.
+
+Lemma hmap_proof (I : eqType) (T_ S_ : I -> Type) (idx : seq I)
+                 (f : forall i, T_ i -> S_ i) (hs : hseq T_ idx) :
+  map tag (map (fun t => Tagged S_ (f (tag t) (tagged t))) hs) == idx.
+Proof. by rewrite -map_comp /funcomp tags_hseq. Qed.
+
+Definition hmap (I : eqType) (T_ S_ : I -> Type) (idx : seq I)
+                (f : forall i, T_ i -> S_ i) (hs : hseq T_ idx) :=
+  HSeq (hmap_proof f hs).
+
+Lemma hmapK (I : eqType) T_ S_ idx
+            (f : forall i : I, T_ i -> S_ i)
+            (g : forall i : I, S_ i -> T_ i) :
+  (forall i, cancel (f i) (g i)) ->
+  cancel (hmap f : hseq T_ idx -> _) (hmap g).
+Proof.
+by move=> Hfg hs; apply: val_inj; apply: mapK=> - [i x] /=; rewrite Hfg.
+Qed.
+
+Fixpoint split_tuple T ns :
+  (sumn ns).-tuple T -> hseq (fun n => n.-tuple T) ns :=
+  match ns with
+  | [::] => fun t => [hseq of [::]]
+  | n :: ns' =>
+    fun t =>
+      let t1 := [tuple of take n t] in
+      let ts := [tuple of drop n t] in
+      [hseq of Tagged _ (tcast (minn_idPl (leq_addr (sumn ns') n)) t1) ::
+                        split_tuple (tcast (addKn n (sumn ns')) ts)]
+  end.
+
+Fixpoint merge_tuple T ns :
+  hseq (fun n => n.-tuple T) ns -> (sumn ns).-tuple T :=
+  match ns with
+  | [::] => fun hs => [tuple]
+  | n :: ns' => fun hs => [tuple of hshead hs ++ merge_tuple [hseq of behead hs]]
+  end.
+
+Lemma merge_tupleK T ns : cancel (@merge_tuple T ns) (@split_tuple T ns).
+Proof.
+elim: ns => [|n ns IH] hs //=; first by rewrite hseq_nil [in RHS]hseq_nil.
+case/hseqP: hs=> t hs /=; rewrite hsheadE /=.
+apply: val_inj=> /=; congr cons.
+  congr Tagged.
+  set t' := [tuple of take _ _]; move: (minn_idPl _)=> E.
+  have : val t' = val t by rewrite /t' /= take_size_cat //= size_tuple.
+  by move: (E) t'; rewrite {}E=> E t'; rewrite tcast_id; apply: val_inj.
+set t' := [tuple of drop _ _]; move: (addKn _ _) => E.
+suff -> : tcast E t' = merge_tuple hs by rewrite IH.
+have : val t' = val (merge_tuple hs).
+  rewrite /t' /= drop_size_cat // ?size_tuple // mkhseqE //=.
+  by congr merge_tuple; apply: val_inj.
+by move: (E) t'; rewrite {}E=> E t'; rewrite tcast_id; apply: val_inj.
+Qed.
+
+Lemma split_tupleK T ns : cancel (@split_tuple T ns) (@merge_tuple T ns).
+Proof.
+elim: ns => [|n ns IH] /= t //=; first by rewrite [in RHS]tuple0.
+have H : forall m (E : m = n) t', val (tcast E t') = val t'.
+  by move=> T' m E; move: (E); rewrite {}E => E t'; rewrite tcast_id.
+rewrite hsheadE; apply: val_inj=> /=; rewrite !{}H /=.
+rewrite -[in RHS](cat_take_drop n t); congr cat.
+rewrite -[drop n t]/(val [tuple of drop n t]).
+move: (addKn _ _) [tuple of drop n t] => E; move: (E); rewrite {}E=> E t'.
+by rewrite tcast_id -[in RHS](IH t') /=; congr merge_tuple; apply: val_inj.
+Qed.
