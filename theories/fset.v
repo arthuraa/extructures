616c42c3dfb5c5abf5528fe030f84d51920f9099
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2015-03-05 16:04:25 -0500

    Implicit arguments declaration.

--- theories/fset.v
+++ theories/fset.v
@@ -96,6 +96,8 @@ Canonical mem_fset_predType := mkPredType mem_fset.
 End Operations.
 
 Arguments fset0 {_}.
+Arguments fsetU1 {_} _ _.
+Arguments fsetU {_} _ _.
 
 Notation "x |: s" := (fsetU1 s x) : fset_scope.
 Notation "s1 :|: s2" := (fsetU s1 s2) : fset_scope.
