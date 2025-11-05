eaae009c5fb8ed1bedc9f391e8e2e7659195e0f2
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2025-11-05 13:38:06 -0500

    Update nixpkgs.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Description for the project";
+  description = "Functions and sets with extensional reasoning";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
